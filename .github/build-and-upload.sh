cbaef4cf829339297fcca2e6b21e6c96c717dce5
Osama Almaani <osama.maani@gmail.com>
2023-09-27 12:19:25 -0700

    Update build-and-upload.sh

--- .github/build-and-upload.sh
+++ .github/build-and-upload.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "Buidling and pushing israelhikingmap/graphhopper:latest"
+echo "Buidling and pushing oalmaani/graphhopper:latest"
 ./build.sh --push
 
 TAG=`cd graphhopper; git for-each-ref --sort=committerdate refs/tags | sed -n '$s/.*\///p'`
-if docker manifest inspect "israelhikingmap/graphhopper:${TAG}" >/dev/null; then 
+if docker manifest inspect "oalmaani/graphhopper:${TAG}" >/dev/null; then 
     echo "No need to push existing version: ${TAG}";
 else
-    echo "Buidling and pushing israelhikingmap/graphhopper:${TAG}"
+    echo "Buidling and pushing oalmaani/graphhopper:${TAG}"
     ./build.sh --push "${TAG}"
 fi
